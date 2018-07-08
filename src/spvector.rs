5949ad1427c4c9fae4c21e5229da69dc73c37909
Giovanni Bajo <rasky@develer.com>
2018-07-09 00:51:14 +0200

    spvector: add V{MUL,MAC}{F,U}

--- src/spvector.rs
+++ src/spvector.rs
@@ -141,6 +141,137 @@ impl<'a> Vectorop<'a> {
     }
 }
 
+unsafe fn to_u128(x: __m128i) -> u128 {
+    let v: [u8; 16] = [0u8; 16];
+    _mm_store_si128(v.as_ptr() as *mut _, x);
+    LittleEndian::read_u128(&v)
+}
+
+macro_rules! accum_shl {
+    ($lo:ident, $md:ident, $hi:ident, $count:expr) => {{
+        const COUNT: i32 = $count;
+        const RCOUNT: i32 = 16 - $count;
+
+        let lo2 = _mm_srli_epi16($lo, RCOUNT);
+        let reslo = _mm_slli_epi16($lo, COUNT);
+        let md2 = _mm_srli_epi16($md, RCOUNT);
+        let resmd = _mm_or_si128(_mm_slli_epi16($md, COUNT), lo2);
+        let reshi = _mm_or_si128(_mm_slli_epi16($hi, COUNT), md2);
+        (reslo, resmd, reshi)
+    }};
+}
+
+macro_rules! _mm_add_epi16_carry {
+    ($a:expr, $b:expr) => {{
+        let va = $a;
+        let vb = $b;
+        let mask = _mm_set1_epi16(-32768i16);
+        let res = _mm_add_epi16(va, vb);
+        let carry = _mm_srli_epi16(
+            _mm_cmpgt_epi16(_mm_xor_si128(va, mask), _mm_xor_si128(res, mask)),
+            15,
+        );
+        (res, carry)
+    }};
+}
+
+macro_rules! accum_add {
+    ($aclo:expr, $acmd:expr, $achi:expr, $lo:expr, $md:expr, $hi:expr) => {{
+        let (aclo, carry1) = _mm_add_epi16_carry!($aclo, $lo);
+        let (acmd, carry2) = _mm_add_epi16_carry!($acmd, $md);
+        let achi = _mm_add_epi16($achi, $hi);
+        let (acmd, carry3) = _mm_add_epi16_carry!(acmd, carry1);
+        let achi = _mm_add_epi16(achi, carry2);
+        let achi = _mm_add_epi16(achi, carry3);
+        (aclo, acmd, achi)
+    }};
+}
+
+macro_rules! accum_add2 {
+    ($aclo:expr, $acmd:expr, $lo:expr, $md:expr, $signed:expr) => {{
+        let (aclo, carry1) = _mm_add_epi16_carry!($aclo, $lo);
+        let acmd = if $signed {
+            _mm_adds_epi16($acmd, $md)
+        } else {
+            _mm_add_epi16($acmd, $md)
+        };
+        let acmd = _mm_adds_epi16(acmd, carry1);
+        (aclo, acmd)
+    }};
+}
+
+#[inline(always)]
+unsafe fn vmulf(
+    vs: __m128i,
+    vt: __m128i,
+    aclo: __m128i,
+    acmd: __m128i,
+    achi: __m128i,
+    signed: bool,
+    mac: bool,
+) -> (__m128i, __m128i, __m128i, __m128i) {
+    let vzero = unsafe { _mm_setzero_si128() };
+    let plo = _mm_mullo_epi16(vs, vt);
+    let pmd = _mm_mulhi_epi16(vs, vt);
+    let phi = vzero;
+
+    // Left-shift accumulator by 1
+    let (plo, pmd, phi) = accum_shl!(plo, pmd, phi, 1);
+
+    let (plo, pmd) = if mac {
+        accum_add2!(aclo, acmd, plo, pmd, signed)
+    } else {
+        (plo, pmd)
+    };
+
+    // Add 0x8000 to accumulator md/lo (if not VMAC)
+    let (plo, pmd) = if !mac {
+        let round = _mm_set1_epi16(-32768i16); // 0x8000
+        let (plo1, locarry) = _mm_add_epi16_carry!(plo, round);
+        let pmd1 = _mm_add_epi16(pmd, locarry);
+        (plo1, pmd1)
+    } else {
+        (plo, pmd)
+    };
+
+    // acchi = sign-extend(mid)
+    let phi = if mac && !signed {
+        vzero
+    } else {
+        _mm_cmpgt_epi16(vzero, pmd)
+    };
+
+    let res = if signed {
+        pmd
+    } else {
+        // Clamp unsigned -> reject negative values
+        if !mac {
+            _mm_and_si128(_mm_cmpgt_epi16(pmd, vzero), pmd)
+        } else {
+            _mm_or_si128(_mm_cmplt_epi16(pmd, vzero), pmd)
+        }
+    };
+
+    (res, plo, pmd, phi)
+}
+
+macro_rules! op_vmulf {
+    ($op:expr,signed($signed:expr),mac($mac:expr)) => {{
+        let (res, acc_lo, acc_md, acc_hi) = vmulf(
+            $op.vs(),
+            $op.vte(),
+            $op.accum(0),
+            $op.accum(1),
+            $op.accum(2),
+            $signed,
+            $mac,
+        );
+        $op.setvd(res);
+        $op.setaccum(0, acc_lo);
+        $op.setaccum(1, acc_md);
+        $op.setaccum(2, acc_hi);
+    }};
+}
 impl Cop for SpVector {
     fn reg(&self, idx: usize) -> u128 {
         match idx {
@@ -171,6 +302,10 @@ impl Cop for SpVector {
         if op.op & (1 << 25) != 0 {
             unsafe {
                 match op.func() {
+                    0x00 => op_vmulf!(op, signed(true), mac(false)), // VMULF
+                    0x01 => op_vmulf!(op, signed(false), mac(false)), // VMULU
+                    0x08 => op_vmulf!(op, signed(true), mac(true)),  // VMACF
+                    0x09 => op_vmulf!(op, signed(false), mac(true)), // VMACU
                     0x10 => {
                         // VADD
                         let vs = op.vs();
