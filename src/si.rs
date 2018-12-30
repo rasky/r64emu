7e1730951dbcfdb8866db97aba3c4c38671c7fa2
Giovanni Bajo <rasky@develer.com>
2018-12-30 15:08:04 +0100

    si: more logging

--- src/si.rs
+++ src/si.rs
@@ -6,7 +6,7 @@ use emu_derive::DeviceBE;
 
 #[derive(DeviceBE)]
 pub struct Si {
-    #[reg(bank = 0, offset = 0x18, rwmask = 0, wcb)]
+    #[reg(bank = 0, offset = 0x18, rwmask = 0, rcb, wcb)]
     status: Reg32,
 
     logger: slog::Logger,
@@ -20,7 +20,12 @@ impl Si {
         })
     }
 
+    fn cb_read_status(&self, val: u32) -> u32 {
+        error!(self.logger, "read SI status reg"; "val" => val.hex());
+        val
+    }
+
     fn cb_write_status(&self, _old: u32, new: u32) {
-        error!(self.logger, "write SI status reg"; o!("val" => new.hex()));
+        error!(self.logger, "write SI status reg"; "val" => new.hex());
     }
 }
