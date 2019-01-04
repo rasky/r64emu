23688cad908760391a48b2e7aa238baeb4219a5b
Giovanni Bajo <rasky@develer.com>
2019-01-04 17:19:40 +0100

    mi: implement mode register (for DP irq ack), and fix irq generation by using the mask

--- src/mi.rs
+++ src/mi.rs
@@ -1,5 +1,6 @@
 use super::n64::R4300;
 use emu::bus::be::{Device, Reg32};
+use emu::int::Numerics;
 use mips64::Cop0;
 
 use bit_field::BitField;
@@ -19,6 +20,17 @@ bitflags! {
 
 #[derive(DeviceBE)]
 pub struct Mi {
+    // 0x04300000 to 0x04300003  MI_INIT_MODE_REG or MI_MODE_REG //MI init mode
+    // (W): [0-6] init length        (R): [0-6] init length
+    //  [7] clear init mode           [7] init mode
+    //  [8] set init mode             [8] ebus test mode
+    //  [9/10] clr/set ebus test mode [9] RDRAM reg mode
+    //  [11] clear DP interrupt
+    //  [12] clear RDRAM reg
+    //  [13] set RDRAM reg mode
+    #[reg(offset = 0x00, wcb)]
+    reg_mode: Reg32,
+
     #[reg(offset = 0x08, readonly)]
     irq_ack: Reg32,
 
@@ -31,31 +43,63 @@ pub struct Mi {
 impl Mi {
     pub fn new(logger: slog::Logger) -> Box<Mi> {
         Box::new(Mi {
+            reg_mode: Reg32::default(),
             irq_ack: Reg32::default(),
             irq_mask: Reg32::default(),
             logger,
         })
     }
 
-    pub fn set_irq_line(&mut self, mask: IrqMask, status: bool) {
+    fn cb_write_reg_mode(&mut self, old: u32, new: u32) {
+        let mut mode = old;
+
+        mode.set_bits(0..7, new.get_bits(0..7));
+
+        if new.get_bit(7) {
+            // clear init mode
+            mode.set_bit(7, false);
+        }
+        if new.get_bit(8) {
+            // set init mode
+            mode.set_bit(7, true);
+        }
+        if new.get_bit(9) {
+            // clear ebus
+            mode.set_bit(8, false);
+        }
+        if new.get_bit(10) {
+            // set ebus
+            mode.set_bit(8, true);
+        }
+        if new.get_bit(11) {
+            // clear RDP interrupt
+            self.set_irq_line(IrqMask::DP, false);
+        }
+        if new.get_bit(12) {
+            // clear ebus
+            mode.set_bit(9, false);
+        }
+        if new.get_bit(13) {
+            // set ebus
+            mode.set_bit(9, true);
+        }
+        self.reg_mode.set(mode);
+        info!(self.logger, "written reg_mode"; "mode" => mode.hex());
+    }
+
+    pub fn set_irq_line(&mut self, lines: IrqMask, status: bool) {
         let old = self.irq_ack.get();
         let new = if status {
-            old | mask.bits()
+            old | lines.bits()
         } else {
-            old & !mask.bits()
+            old & !lines.bits()
         };
         self.irq_ack.set(new);
+
         if old != new {
             info!(self.logger, "changed IRQ ack"; "irq" => ?IrqMask::from_bits(new));
-
-            // FIXME: this reentrancy will eventually panic (CPU -> BUS -> Device -> CPU).
-            // Find out a way to fix this.
-            if new != 0 {
-                R4300::get_mut().cop0.set_hwint_line(0, true);
-            } else {
-                R4300::get_mut().cop0.set_hwint_line(0, false);
-            }
         }
+        self.update_cpu_irq();
     }
 
     fn cb_write_irq_mask(&mut self, old: u32, new: u32) {
@@ -66,6 +110,15 @@ impl Mi {
             }
         }
         self.irq_mask.set(mask);
-        info!(self.logger, "changed IRQ mask"; "irq" => ?IrqMask::from_bits(mask));
+        if old != mask {
+            info!(self.logger, "changed IRQ mask"; "irq" => ?IrqMask::from_bits(mask));
+        }
+        self.update_cpu_irq();
+    }
+
+    fn update_cpu_irq(&self) {
+        R4300::get_mut()
+            .cop0
+            .set_hwint_line(0, (self.irq_ack.get() & self.irq_mask.get()) != 0);
     }
 }
