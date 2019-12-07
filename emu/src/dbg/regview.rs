ea624fd9b342124a7af26963ddd6b3b8ec784696
Giovanni Bajo <rasky@develer.com>
2019-12-07 01:24:03 +0100

    sp: display VU registers in more compact form

--- emu/src/dbg/regview.rs
+++ emu/src/dbg/regview.rs
@@ -1,5 +1,5 @@
 use super::uisupport::*;
-use super::{UiCtx, RegHighlight};
+use super::{RegHighlight, UiCtx};
 use imgui::*;
 
 pub enum RegisterSize<'a> {
@@ -7,6 +7,7 @@ pub enum RegisterSize<'a> {
     Reg16(&'a mut u16),
     Reg32(&'a mut u32),
     Reg64(&'a mut u64),
+    Reg16x8(&'a mut [u16; 8]),
 }
 
 /// A trait for an object that can display register contents to
@@ -21,9 +22,9 @@ pub trait RegisterView {
         F: for<'a> FnMut(&'a str, RegisterSize<'a>, Option<&str>);
 }
 
-const COLOR_BG_NORMAL: [f32; 4] = [41.0/255.0,74.0/255.0,122.0/255.0,138.0/255.0];
-const COLOR_BG_INPUT: [f32; 4] = [86.0/255.0,171.0/255.0,60.0/255.0,138.0/255.0];
-const COLOR_BG_OUTPUT: [f32; 4] = [204.0/255.0,61.0/255.0,61.0/255.0,138.0/255.0];
+const COLOR_BG_NORMAL: [f32; 4] = [41.0 / 255.0, 74.0 / 255.0, 122.0 / 255.0, 138.0 / 255.0];
+const COLOR_BG_INPUT: [f32; 4] = [86.0 / 255.0, 171.0 / 255.0, 60.0 / 255.0, 138.0 / 255.0];
+const COLOR_BG_OUTPUT: [f32; 4] = [204.0 / 255.0, 61.0 / 255.0, 61.0 / 255.0, 138.0 / 255.0];
 
 pub(crate) fn render_regview<'a, 'ui, RV: RegisterView>(
     ui: &'a Ui<'ui>,
@@ -35,32 +36,48 @@ pub(crate) fn render_regview<'a, 'ui, RV: RegisterView>(
         .size(RV::WINDOW_SIZE, Condition::FirstUseEver)
         .build(ui, || {
             // Iterate on all the columns
-            ui.columns(RV::COLUMNS as _, im_str!("columns"), true);
+            ui.columns(RV::COLUMNS as _, im_str!("##columns"), true);
             for col in 0..RV::COLUMNS {
                 // Visit regs for this column
-                v.visit_regs(col, |name, val, desc| {
+                v.visit_regs(col, |rname, val, desc| {
                     use self::RegisterSize::*;
 
                     // Check if this register requires some special
                     // highlight.
                     let bgcolor = match disasm {
                         None => COLOR_BG_NORMAL,
-                        Some(d) => match d.regs_highlight.get(name) {
+                        Some(d) => match d.regs_highlight.get(rname) {
                             None => COLOR_BG_NORMAL,
                             Some(RegHighlight::Input) => COLOR_BG_INPUT,
                             Some(RegHighlight::Output) => COLOR_BG_OUTPUT,
-                        }
+                        },
                     };
 
                     // Draw the register box
-                    let name = &im_str!("{}", name);
+                    let name = &im_str!("{}", rname);
                     let color = ui.push_style_color(StyleColor::FrameBg, bgcolor);
 
                     match val {
-                        Reg8(v) => imgui_input_hex(ui, name, v, true),
-                        Reg16(v) => imgui_input_hex(ui, name, v, true),
-                        Reg32(v) => imgui_input_hex(ui, name, v, true),
-                        Reg64(v) => imgui_input_hex(ui, name, v, true),
+                        Reg8(v) => {
+                            imgui_input_hex(ui, name, v, true);
+                        }
+                        Reg16(v) => {
+                            imgui_input_hex(ui, name, v, true);
+                        }
+                        Reg32(v) => {
+                            imgui_input_hex(ui, name, v, true);
+                        }
+                        Reg64(v) => {
+                            imgui_input_hex(ui, name, v, true);
+                        }
+                        Reg16x8(v) => {
+                            let left = ui.cursor_pos()[0];
+                            for i in 0..7 {
+                                imgui_input_hex(ui, &im_str!("##{}#{}", rname, i), &mut v[i], true);
+                                ui.same_line(left + (i + 1) as f32 * 40.0);
+                            }
+                            imgui_input_hex(ui, name, &mut v[7], true);
+                        }
                     };
                     if let Some(desc) = desc {
                         ui.text(im_str!("{}", desc));
--- src/sp/cop2.rs
+++ src/sp/cop2.rs
@@ -952,7 +952,7 @@ impl Cop for SpCop2 {
 
 impl dbg::RegisterView for SpCop2 {
     const WINDOW_SIZE: [f32; 2] = [180.0, 400.0];
-    const COLUMNS: usize = 8;
+    const COLUMNS: usize = 1;
 
     fn name(&self) -> &str {
         &self.name
@@ -971,14 +971,27 @@ impl dbg::RegisterView for SpCop2 {
         const REG_NAMES: [&str; 32] = [ "v0","v1","v2","v3","v4","v5","v6","v7","v8","v9","v10","v11","v12","v13","v14","v15","v16","v17","v18","v19","v20","v21","v22","v23","v24","v25","v26","v27","v28","v29","v30","v31"];
         const ACC_NAMES: [&str; 3] = [ "acc_lo", "acc_md", "acc_hi"];
 
+        let mut vle: [u16; 8] = [0; 8];
         for i in 0..32 {
             let v: &mut [u16; 8] = unsafe { std::mem::transmute(&mut ctx.vregs[i].0) };
-            visit(if col==7 { &REG_NAMES[i] } else { "" }, Reg16(&mut v[7-col]), None);
+            for j in 0..8 {
+                vle[j] = v[7-j];
+            }
+            visit(&REG_NAMES[i], Reg16x8(&mut vle), None);
+            for j in 0..8 {
+                v[j] = vle[7-j];
+            }
         }
 
         for i in 0..3 {
             let v: &mut [u16; 8] = unsafe { std::mem::transmute(&mut ctx.accum[i].0) };
-            visit(if col==7 { ACC_NAMES[i] } else { "" }, Reg16(&mut v[7-col]), None);
+            for j in 0..8 {
+                vle[j] = v[7-j];
+            }
+            visit(ACC_NAMES[i], Reg16x8(&mut vle), None);
+            for j in 0..8 {
+                v[j] = vle[7-j];
+            }
         }
     }
 }
